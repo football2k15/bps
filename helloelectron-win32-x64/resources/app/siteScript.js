f3901b2387ef90d5c0ec193c60d30e068c3fb4f8
Elijah <elijah_j@bigbrandtire.com>
2017-10-09 19:32:04 -0700

    Finally got it to build withnpm version 5.2

---
+++ helloelectron-win32-x64/resources/app/siteScript.js
@@ -0,0 +1,25 @@
+// 'use strict';
+
+// const electron = require('electron');
+// const app = electron.app;
+
+// const path = electron.path;
+// const url = electron.url;
+// const BrowserWindow = electron.BrowserWindow;
+// var fs = electron.fs;
+// var ipc = electron.ipcMain;
+// const Menu = electron.Menu;
+
+// var menu = Menu.buildFromTemplate([{
+// label: 'Electron',
+// submenu:[
+//     {
+//         label: 'Prefs',
+//         click: function(){
+//             ipc.send('show-prefs');
+//         }
+//     }
+// ]
+// }]);
+// Menu.setApplicationMenu(menu);
+
