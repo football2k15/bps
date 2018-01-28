697c5bb47664f0d091f98065af1dafa3e7a822a8
Elijah <elijah_j@bigbrandtire.com>
2018-01-27 16:48:42 -0800

    confirm working state

--- Store.js
+++ Store.js
@@ -20,13 +20,11 @@ class Store {
   
   // ...and this will set it
   set(key, val) {
-      
     this.data[key] = val;
     // Wait, I thought using the node.js' synchronous APIs was bad form?
     // We're not writing a server so there's not nearly the same IO demand on the process
     // Also if we used an async API and our app was quit before the asynchronous write had a chance to complete,
     // we might lose that data. Note that in a real app, we would try/catch this.
-    console.log(this.path)
     fs.writeFileSync(this.path, JSON.stringify(this.data));
   }
 }
